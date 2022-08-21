d1731e5b2060d9f4a99700dc964ace96dca91590
sachintiwari <32461337+sachint2202@users.noreply.github.com>
2022-08-21 18:06:48 +0530

    add code terminal placeholder

--- src/app/Feature/basics/basics-routing.module.ts
+++ src/app/Feature/basics/basics-routing.module.ts
@@ -5,6 +5,7 @@ import { BasicsComponent } from './basics.component';
 const routes: Routes = [{ path: '', component: BasicsComponent }];
 
 @NgModule({
+
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
--- src/app/Feature/basics/basics.component.ts
+++ src/app/Feature/basics/basics.component.ts
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BasicsComponent implements OnInit {
 
+  tabToDisplay: string = 'Promise';
+  codeContent: string = 'ng serve';
+  codeResult: string = 'Code ran successfully...';
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  callTabChange(tab: string){
+    console.log(tab);
+    this.tabToDisplay = tab;
   }
 
 }
