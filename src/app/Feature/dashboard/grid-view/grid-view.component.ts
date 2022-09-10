032f083bfc54f6b95503103b1454b3499921c471
sachintiwari <32461337+sachint2202@users.noreply.github.com>
2022-09-10 12:28:31 +0530

    Add ToDO Grid

--- src/app/Feature/dashboard/grid-view/grid-view.component.ts
+++ src/app/Feature/dashboard/grid-view/grid-view.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, SimpleChanges } from '@angular/core';
 import { ToDoList } from 'src/app/Interfaces/ToDoApiInterface';
 import { BasicCrudService } from 'src/app/Services/basic-crud.service';
 
@@ -10,8 +10,51 @@ import { BasicCrudService } from 'src/app/Services/basic-crud.service';
 export class GridViewComponent implements OnInit {
 
   constructor(private crud:BasicCrudService) { }
-  ToDoData:ToDoList[] = [];
 
+  ToDoData:ToDoList[] = [];
+  addAChore:boolean = false;
+  newTitle = "";
+
+  //Add a new ToDo to the list
+  AddToList(){
+    let newToDo:ToDoList = {
+      userId:0,
+      id:0,
+      title:this.newTitle,
+      completed:false
+    }
+    this.crud.addAToDo(newToDo).subscribe(
+      (val)=> {
+        if(val == false){
+          alert('Please try to save again');
+          return;
+        }
+        console.log('New chore added successfully')
+        this.newTitle="";
+        this.addAChore=false;
+        this.ToDoData.push(val);
+      },
+      
+    )
+  }
+
+  //Update an existing ToDo from the list
+  UpdateToDo(existingToDo:ToDoList){
+    this.crud.updateAToDo(existingToDo)
+    .subscribe(
+      (val) => {
+        if(!val){
+          alert('Please try to update again');
+          return;
+        }
+        alert('Updated successfully');
+        this.ToDoData.map(x => {if(x.id == val.id)x=val})
+      }
+    )
+
+  }
+
+  //Get Data Initially From Fake APIs
   ngOnInit(): void {
    this.crud.getFakeData().subscribe(x => this.ToDoData = x);
   }
--- src/app/Services/basic-crud.service.ts
+++ src/app/Services/basic-crud.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import {HttpClient, HttpClientModule} from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { catchError, Observable, of } from 'rxjs';
 import { ToDoList } from '../Interfaces/ToDoApiInterface';
 
 @Injectable({
@@ -11,7 +11,38 @@ export class BasicCrudService {
   constructor(private http:HttpClient) { }
 
   getFakeData():Observable<ToDoList[]>{
-    return this.http.get<ToDoList[]>('https://jsonplaceholder.typicode.com/users/1/todos');
+    return this.http.get<ToDoList[]>('https://jsonplaceholder.typicode.com/users/1/todos')
+    .pipe(
+      catchError(
+        this.handleError('getting To Do List',[])
+        )
+    );
+  }
+
+  addAToDo(newToDo:ToDoList):Observable<any>{
+    return this.http.post('https://jsonplaceholder.typicode.com/users',
+    newToDo).pipe(
+      catchError(
+        this.handleError('creating new To Do',false)
+        )
+    );
+  }
+
+  updateAToDo(existingToDo:ToDoList):Observable<any>{
+    return this.http.put('https://jsonplaceholder.typicode.com/users/'+existingToDo.id,
+    existingToDo).pipe(
+      catchError(
+        this.handleError('updating existing To Do',false)
+        )
+    );
+  }
+
+  private handleError<T>(operation:string,defaultResult:T){
+    return ():Observable<T> =>{
+      console.log('Error in operation - ',operation);
+      return of(defaultResult as T);
+    }
+   
   }
 }
 
